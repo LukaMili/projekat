8ecbcf0063ee912c66db1884b5763f6654795cdf
LukaMili <38590952+LukaMili@users.noreply.github.com>
2018-07-29 04:42:13 +0200

    podeljen home view na admina i korisnika
    Dodati dugmici za korisnika
    -uplati sredstva,pregled kupovina, moja korpa
    Dodati dugmici za admina
    -pregled korisnika,pregled proizovda, pregled kupovina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,32 @@
     
         <div class="jumbotron">
             Ulogovani ste kao {{auth()->user()->role}}
+            <br><br>
+            Stanje na racunu {{auth()->user()->stanjeRacuna}}
+            
+            <a href=""> <div class="btn btn-sm btn-secondary">Uplatite sredstva</div></a>
+            <br><br>
+              Pregled kupovina i racuna
+             <a href=""> <div class="btn btn-sm btn-secondary">Pregled kupovina</div></a>
+             <br><br>
+              Moja korpa
+             <a href=""> <div class="btn btn-sm btn-secondary">Moja korpa</div></a>
         </div>
 
     @else
     <!--PRIKAZ ZA ADMINA -->
 
        <div class="jumbotron">
-            Zdravo admine
+           Zdravo admine <br>
+            
+            Pregled korisnika i promovisanje uloge
+             <a href=""> <div class="btn btn-sm btn-secondary">Pregled korisnika</div></a>
+             <br><br>
+              Pregled proizvoda i dodavanje novih
+             <a href=""> <div class="btn btn-sm btn-secondary">Pregled proizvoda</div></a>
+             <br><br>
+              Pregled kupovina i racuna
+             <a href=""> <div class="btn btn-sm btn-secondary">Pregled kupovina</div></a>
         </div>
     
     @endif
