244a2ac4bd7b4e96fef691e1e130347cc2fc0a28
LukaMili <38590952+LukaMili@users.noreply.github.com>
2018-09-11 03:35:03 +0200

    Izbaceni dugmici iz naloga, zamenjeni slikama sa blur efektom

--- resources/views/pages/nalog.blade.php
+++ resources/views/pages/nalog.blade.php
@@ -6,53 +6,102 @@
     @if(Auth::user()->role=='korisnik')
     <!--PRIKAZ ZA KORISNIKA -->
     
-        <div class="jumbotron">
+    <div class="jumbotron" style="background-color: beige">
             <div class="row">
                 <div class="col md-6 offset-2">
                     Ulogovani ste kao <strong><span style="color: goldenrod">{{Auth::user()->role}}</span> {{auth()->user()->name}}</strong>
                 </div>  
             </div>
             <br><br>
+            
+            
             <div class="row">
-                <div class="col md-6">
-            Stanje na racunu: <strong>{{auth()->user()->stanjeRacuna}} din</strong> 
-                </div>
-                <div class="col md-6">
+                <div class="slikacont" style="background-color: aquamarine; width: 75%;margin: 0 auto;">
                     
-                       <button class="btn btn-sm btn-secondary" type="button" data-toggle="modal" data-target="#myModal" id="dugme">Uplati sredstva</button>
+                
+                        <a data-toggle="modal" data-target="#myModal"><img id="slikaforma" src="{{asset('slike/resp-main.jpg')}}" width="100%" height="330px" alt="Proizvodi" onclick="submit" style="cursor: pointer">
+                            <div class="centriran" style="cursor: pointer">
+                                    <h1 style="">Stanje Na Racunu</h1>
+                                    <p class="small">Kliknite da dodate sredstva</p>
+                                </div></a>
+                        
+                                <div class="doledesno">
+                                    <p style="font-size: large">Trenutno stanje: {{Auth::user()->stanjeRacuna}} dinara<p>
+                                </div>
+                  
                     
                 </div>
             </div>
             
+            
             <br><br>
             
             <div class="row">
-                <div class="col md-6">
-            Pregled kupovina i racuna
-                </div>
-                <div class="col md-6">
-                    <form method="post" action="/nalog/prikaziracune">
-                        {{csrf_field()}}
-                       <button class="btn btn-sm btn-secondary" type="submit" id="dugme">Pregled kupovina</button>
+               <div class="slikacont" style="background-color: aquamarine; width: 75%;margin: 0 auto;">
+                    <form id="formaAdminKorisnici3" method="post" action="/nalog/prikaziracune">
+                {{csrf_field()}}
+                <input type="hidden" value="{{Auth::user()->id}}" name="id">
+                        <img id="slikaforma3" src="{{asset('slike/racun.JPG')}}" width="100%" height="330px" alt="Proizvodi" style="cursor: pointer">
+                                <div class="centriran">
+                                    <h1 style="color:white ;text-shadow: 2px 0 0 #000, -2px 0 0 #000, 0 2px 0 #000, 0 -2px 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000;">Racuni</h1>
+                                    <p class="small">Kliknite da pogledate Vase racune</p>
+                               </div>
+                  
                     </form>
                 </div>
             </div>
             
              <br><br>
+             
+             
              <div class="row">
-                <div class="col md-6">
-              Moja korpa
-                </div>
-                 
-                <div class="col md-6">
-                    <form method="get" action="/nalog/korpa">
-                    <button type="submit" class="btn btn-sm btn-secondary" id="dugme">Moja korpa</button>
+               <div class="slikacont" style="background-color: aquamarine; width: 75%;margin: 0 auto;">
+                    <form id="formaAdminKorisnici4" method="get" action="/nalog/korpa">
+                {{csrf_field()}}
+                <input type="hidden" value="{{Auth::user()->id}}" name="id">
+                        <img id="slikaforma4" src="{{asset('slike/korpa.JPG')}}" width="100%" height="330px" alt="Proizvodi" style="cursor: pointer">
+                                <div class="centriran">
+                                    <h1 style="color:white ;text-shadow: 2px 0 0 #000, -2px 0 0 #000, 0 2px 0 #000, 0 -2px 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000;">Moja Korpa</h1>
+                                    <p class="small">Kliknite da pogledate Vasu korpu</p>
+                               </div>
+                  
                     </form>
                 </div>
                  
             </div>
         </div>
 
+    
+    <script>
+           
+           var form3 = document.getElementById("formaAdminKorisnici3");
+           var form4 = document.getElementById("formaAdminKorisnici4");
+ 
+           document.getElementById("slikaforma3").addEventListener("click", function () {
+            form3.submit();
+           });
+           document.getElementById("slikaforma4").addEventListener("click", function () {
+            form4.submit();
+           });
+           
+           function proveri(){
+               var input=document.getElementById("iznos").value;
+               var regex= /^[0-9]+$/;
+               var rez=regex.test(input);
+               
+               if (rez==false){
+                   return false;
+                   document.getElementById("alert").removeAttribute('hidden');
+               }
+               else{
+                   return true;
+               }
+           }
+           
+       </script>
+    
+    
+    
     @else
         
     <!--PRIKAZ ZA ADMINA -->
@@ -98,11 +147,12 @@
         </div>
         
 
-    
+    @endif
     
        <script>
            var form = document.getElementById("formaAdminKorisnici");
            var form2 = document.getElementById("formaAdminKorisnici2");
+           var form3 = document.getElementById("formaAdminKorisnici3");
            
             document.getElementById("slikaforma").addEventListener("click", function () {
             form.submit();
@@ -112,9 +162,29 @@
             form2.submit();
            });
            
+           document.getElementById("slikaforma3").addEventListener("click", function () {
+            form3.submit();
+           });
+           
+           
+            function proveri(){
+               var input=document.getElementById("iznos").value;
+               var regex= /^[0-9]+$/;
+               
+               
+               if (regex.test(input)){
+                   return true;
+                   
+               }
+               else{                
+                   document.getElementById("alert").removeAttribute('hidden');
+                   return false;
+               }
+           }
+           
        </script>
         
-    @endif
+    
     
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
@@ -130,11 +200,13 @@
       <div class="modal-body">
           <form method="post" action="{{action('KorisnikController@DoplatiNovac')}}" style="text-align: center">
               {{csrf_field()}}
-              <input type="text" placeholder="Unesite cifru" name="iznos" style="width: 80%;height: 37px;margin-left: auto">
-          
+              <input type="text" placeholder="Unesite cifru" autocomplete="off" id="iznos" name="iznos" style="width: 80%;height: 37px;margin-left: auto">
+              <p id="alert" hidden="" style="color: crimson">
+                  Unesite samo cifre!
+              </p>
       </div>
       <div class="modal-footer">
-          <button type="submit" class="btn btn-secondary" style="width: 33%;margin: 0 auto;">Uplati</button>
+          <button type="submit" onclick="return proveri()" class="btn btn-secondary" style="width: 33%;margin: 0 auto;">Uplati</button>
         
       </div>
     </form>
--- resources/views/pages/pocetna.blade.php
+++ resources/views/pages/pocetna.blade.php
@@ -3,8 +3,17 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="wrapper" style="background-color: beige; margin-top: 0; text-align: center">
-   
+<div class="jumbotron" style="background-color: beige;">
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
    @if(!Auth::user())
    <p> NAPRAVI PRIKAZ ZA GOSTA</p>
    
